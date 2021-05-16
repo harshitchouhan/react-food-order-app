e7ef98a6e700913b90738db995c40684f1e6cd34
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2021-05-16 22:16:03 +0530

    Cart Button Component Created

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,7 @@
 import { Fragment } from 'react';
 
+import HeaderCartButton from './HeaderCartButton';
+
 import classes from './Header.module.css';
 
 import mealsImg from './../../assets/meals.webp';
@@ -9,7 +11,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton></HeaderCartButton>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImg} alt="A table full of delicious food!" />
